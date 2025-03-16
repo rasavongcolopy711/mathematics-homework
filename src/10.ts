aeec34e3a68670abb3c685aa20d708a8ea3d7507
rasavongcolopy711 <rasavongcolopy711@gmail.com>
2025-03-16 19:12:52 +0000

    Add 10.ts

---
+++ src/10.ts
@@ -0,0 +1,9 @@
+// Randomly generated math homework problem
+let x = Math.floor(Math.random() * 10);
+let y = Math.floor(Math.random() * 10);
+let z = Math.floor(Math.random() * 10);
+let answer = parseInt((x + y) / z);
+console.log("Solve for x: " + x);
+console.log("Solve for y: " + y);
+console.log("Solve for z: " + z);
+console.log("Answer: " + answer);
